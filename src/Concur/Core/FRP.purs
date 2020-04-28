f4084b080cc32c14e2bcd1eeb3c98082a5af186b
Anupam Jain <ajnsit@gmail.com>
2020-04-29 04:07:53 +0530

    Generalise Signal to SignalT (#3)
    Keep `Signal` specialised to `Widget` for backward compatibility.

--- src/Concur/Core/FRP.purs
+++ src/Concur/Core/FRP.purs
@@ -2,9 +2,9 @@ module Concur.Core.FRP where
 
 import Prelude
 
-import Concur.Core (Widget)
-import Control.Alt ((<|>))
-import Control.Alternative (empty)
+import Concur.Core.Types (Widget)
+import Control.Alt (class Alt, (<|>))
+import Control.Alternative (class Alternative, class Plus, empty)
 import Control.Cofree (Cofree, mkCofree, tail)
 import Control.Comonad (extract)
 import Data.Either (Either(..), either, hush)
@@ -12,8 +12,8 @@ import Data.Maybe (Maybe(..))
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
 import Effect.Aff (delay)
-import Effect.Aff.Class (liftAff)
-import Effect.Class (liftEffect)
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Class (class MonadEffect, liftEffect)
 
 ----------
 -- SIGNALS
@@ -23,34 +23,39 @@ import Effect.Class (liftEffect)
 -- | A Widget can be considered to be a one-shot Event. (There is no stream of events in Concur).
 -- | Signals then are never-ending widget loops that allow access to their last return value.
 -- | This last produced value allows composition with other widgets even for never-ending widgets.
-type Signal v a
-  = Cofree (Widget v) a
+type SignalT m a = Cofree m a
+
+-- | A Signal specific to Widgets
+type Signal v a = SignalT (Widget v) a
 
 -- | Construct a signal from an initial value, and a step widget
 step ::
-  forall v a.
+  forall m a.
   a ->
-  Widget v (Signal v a) ->
-  Signal v a
+  m (SignalT m a) ->
+  SignalT m a
 step = mkCofree
 
 -- | Display a widget which returns a continuation
-display :: forall v. Widget v (Signal v Unit) -> Signal v Unit
+display :: forall m. m (SignalT m Unit) -> SignalT m Unit
 display w = step unit w
 
 -- | Fires a widget once then stop. This will reflow when a parent signal reflows
 -- | Starts as Nothing. Then switches to `Just returnVal` after the Widget is done
-fireOnce :: forall v a. Monoid v => Widget v a -> Signal v (Maybe a)
+fireOnce :: forall m a. Monad m => Plus m => m a -> SignalT m (Maybe a)
 fireOnce w = step Nothing do
   a <- w
   pure (step (Just a) empty)
 
 -- | Similar to `fireOnce`, but discards the return value
-fireOnce_ :: forall v. Monoid v => Widget v Unit -> Signal v Unit
+fireOnce_ :: forall m. Monad m => Plus m => m Unit -> SignalT m Unit
 fireOnce_ w = display do w *> empty
 
 -- | Wait until we get a `Just` value from a signal
-justWait :: forall v a b. Monoid v => b -> Signal v (Maybe a) -> (a -> Signal v b) -> Signal v b
+justWait :: forall m a b.
+            Monad m =>
+            Alternative m =>
+            b -> SignalT m (Maybe a) -> (a -> SignalT m b) -> SignalT m b
 justWait b s f = do
   m <- s
   case m of
@@ -58,60 +63,63 @@ justWait b s f = do
     Just a -> f a
 
 -- | Run an effectful computation, and do something with the result
-justEffect :: forall v a b. Monoid v => b -> Effect a -> (a -> Signal v b) -> Signal v b
+justEffect :: forall m a b. MonadEffect m => Monad m => Alternative m => b -> Effect a -> (a -> SignalT m b) -> SignalT m b
 justEffect b e f = justWait b (fireOnce do liftEffect e) f
 
 -- | A constant signal
 always ::
-  forall v a.
-  Monoid v =>
+  forall m a.
+  Monad m =>
+  Alternative m =>
   a ->
-  Signal v a
+  SignalT m a
 always = pure
 
 -- | Update signal to a new value
 update ::
-  forall v a.
-  Signal v a ->
-  Widget v (Signal v a)
+  forall m a.
+  SignalT m a ->
+  m (SignalT m a)
 update = tail
 
 -- | Construct a signal by polling a signal with a nested widget for values
 poll ::
-  forall v a.
-  Signal v (Widget v a) ->
-  Widget v (Signal v a)
+  forall m a.
+  Monad m =>
+  SignalT m (m a) ->
+  m (SignalT m a)
 poll b = step <$> extract b <*> (map poll (update b))
 
 -- | Create a signal which repeatedly invokes a widget for values.
 -- | E.g. `signal False checkbox` will return a signal which reflects the current value of the checkbox.
 hold ::
-  forall v a.
-  Monoid v =>
+  forall m a.
+  Monad m =>
   a ->
-  Widget v a ->
-  Signal v a
+  m a ->
+  SignalT m a
 hold a w = step a do
   a' <- w
   pure (hold a' w)
 
 -- | Create a signal which repeatedly invokes a widget function for values, looping in the prev value.
 loopW ::
-  forall v a.
+  forall m a.
+  Monad m =>
   a ->
-  (a -> Widget v a) ->
-  Signal v a
+  (a -> m a) ->
+  SignalT m a
 loopW a f = step a (go <$> f a)
   where
   go x = loopW x f
 
 -- | Loop a signal so that the return value is passed to the beginning again.
 loopS ::
-  forall v a.
-  Monoid v =>
+  forall m a.
+  Monad m =>
   a ->
-  (a -> Signal v a) ->
-  Signal v a
+  (a -> SignalT m a) ->
+  SignalT m a
 loopS a f = step (extract this) do
   s <- update this
   pure (loopS (extract s) f)
@@ -119,7 +127,7 @@ loopS a f = step (extract this) do
   this = f a
 
 -- | Loop a signal so that the return value is passed to the beginning again.
--- loop :: forall v a. Monoid v => (a -> Signal v (Maybe a)) -> Signal v a
+-- loop :: forall m a. Monoid v => (a -> SignalT m (Maybe a)) -> SignalT m a
 -- loop f = step (extract this) do
 --   s <- update this
 --   pure (loopS (extract s) f)
@@ -128,65 +136,72 @@ loopS a f = step (extract this) do
 -- | This can be used to implement simple stateful Signals.
 -- | e.g. `counter = fold (\n _ -> n+1) 0 clicks`
 foldp ::
-  forall v a b.
+  forall m a b.
+  Functor m =>
   (a -> b -> a) ->
   a ->
-  Signal v b ->
-  Signal v a
+  SignalT m b ->
+  SignalT m a
 foldp f a sb = step a' (map (foldp f a') (update sb))
   where
   a' = f a (extract sb)
 
 -- | Consume a closed signal to make a widget
--- dyn :: forall v. (forall x. Signal v x) ~> (forall x. Widget v x)
+-- dyn :: forall v. (forall x. SignalT m x) ~> (forall x. m x)
 dyn ::
-  forall v a b.
-  Signal v a ->
-  Widget v b
+  forall m a b.
+  Monad m =>
+  SignalT m a ->
+  m b
 dyn s = update s >>= dyn
 
 -- | Run a signal *once* and return its value
 oneShot ::
-  forall v a.
-  Signal v (Maybe a) ->
-  Widget v a
+  forall m a.
+  Monad m =>
+  SignalT m (Maybe a) ->
+  m a
 oneShot s = case extract s of
   Nothing -> update s >>= oneShot
   Just a -> pure a
 
 -- Very useful to embed a signal in the middle of a widget
 demand ::
-  forall v a.
-  Signal v (Maybe a) ->
-  Widget v a
+  forall m a.
+  Monad m =>
+  SignalT m (Maybe a) ->
+  m a
 demand = oneShot
 
-demand' :: forall v a. (Maybe a -> Signal v (Maybe a)) -> Widget v a
+demand' :: forall m a. Monad m => (Maybe a -> SignalT m (Maybe a)) -> m a
 demand' f = oneShot (f Nothing)
 
 -- A Common pattern is demand + stateLoopS
 demandLoop ::
-  forall v a s.
-  Monoid v =>
+  forall m a s.
+  Monad m =>
+  Alternative m =>
   s ->
-  (s -> Signal v (Either s a)) ->
-  Widget v a
+  (s -> SignalT m (Either s a)) ->
+  m a
 demandLoop def w = demand (stateLoopS def w)
 
 -- A generalisation of `loopS` where, you have an inner loop state `s` and a final result `a`
 -- The loop continues as long as `Left s` is returned. And ends when `Right a` is returned.
 stateLoopS ::
-  forall v a s.
-  Monoid v =>
+  forall m a s.
+  Monad m =>
+  Alternative m =>
   s ->
-  (s -> Signal v (Either s a)) ->
-  Signal v (Maybe a)
+  (s -> SignalT m (Either s a)) ->
+  SignalT m (Maybe a)
 stateLoopS def w = map hush $ loopS (Left def) $ either w (pure <<< Right)
 
 
 -- Debounced output from a widget
 -- wrapped into a signal
-debounce :: forall a v. Monoid v => Number -> a -> (a -> Widget v a) -> Signal v a
+debounce :: forall m a. Monad m => Alt m => MonadAff m =>
+            Number -> a -> (a -> m a) -> SignalT m a
 debounce timeoutMs ainit winit = go ainit winit
   where
     go a w = step a do
