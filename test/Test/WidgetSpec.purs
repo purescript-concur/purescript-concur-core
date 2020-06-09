43f7cd603a76e770738fe37a312926ab954e5ce4
kamoii <>
2020-06-09 23:14:20 +0900

    Add one more test case

--- test/Test/WidgetSpec.purs
+++ test/Test/WidgetSpec.purs
@@ -5,7 +5,7 @@ import Prelude
 import Concur.Core.Types (affAction)
 import Control.MultiAlternative (orr)
 import Data.Time.Duration (Milliseconds(..))
-import Effect.Aff (delay)
+import Effect.Aff (delay, never)
 import Effect.Class (liftEffect)
 import Effect.Ref as Ref
 import Test.Spec (Spec, describe, it)
@@ -30,3 +30,18 @@ widgetSpec =
         liftEffect (Ref.read ref) `shouldReturn` "a"
         delay (Milliseconds 100.0)
         liftEffect (Ref.read ref) `shouldReturn` "a"
+
+      it "should start all the widgets only once" do
+        ref <- liftEffect (Ref.new 0)
+        { result, views } <- runWidgetAsAff $ orr
+          [ do
+               affAction "a0" $ delay (Milliseconds 100.0)
+               affAction "a1" $ delay (Milliseconds 100.0)
+               pure "a"
+          , affAction "b" do
+               liftEffect $ Ref.modify_ (_ + 1) ref
+               never
+          ]
+        result `shouldEqual` "a"
+        views `shouldEqual` [ "a0b", "a1b" ]
+        liftEffect (Ref.read ref) `shouldReturn` 1
