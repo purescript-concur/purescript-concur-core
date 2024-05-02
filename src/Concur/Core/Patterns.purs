cdd2a0f7e8b5d377bc6097be9084f5f89b888370
Anupam Jain <ajnsit@gmail.com>
2024-05-02 20:13:05 +0530

    Add Concur.Core.Patterns.internalise

--- src/Concur/Core/Patterns.purs
+++ src/Concur/Core/Patterns.purs
@@ -1,7 +1,6 @@
 module Concur.Core.Patterns where
 
 import Concur.Core (Result(..), Widget, mkWidget)
-import Control.Alternative (empty)
 import Control.Applicative (pure)
 import Control.Bind (bind, discard, (>>=))
 import Data.Array as Array
@@ -9,22 +8,24 @@ import Data.Function (($))
 import Data.Monoid (class Monoid)
 import Data.Semigroup ((<>))
 import Data.Traversable (traverse_)
+import Data.Unit (Unit)
 import Data.Void (Void)
-import Effect.Class (liftEffect)
+import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Ref as Ref
 import Unsafe.Reference (unsafeRefEq)
 
 type RemoteInterface v a =
   { yield :: Widget v a
-  , render :: Widget v Void
+  , render :: Widget v Unit
   }
 
 -- | Separate the UI from the widget result
 remoteWidget
-  :: forall v a
+  :: forall m v a
   .  Monoid v
+  => MonadEffect m
   => Widget v a
-  -> Widget v (RemoteInterface v a)
+  -> m (RemoteInterface v a)
 remoteWidget axn = do
   remoteCb <- liftEffect $ Ref.new []
   pure { yield: yield remoteCb, render: render remoteCb }
@@ -37,5 +38,25 @@ remoteWidget axn = do
     val <- axn
     liftEffect do
        Ref.read remoteCb >>= traverse_ \cb -> cb (Completed val)
-    empty
+
+-- | Internalise state
+-- The resulting widget can be rescheduled multiple
+-- times and will retain its state internally
+internalise
+  :: forall m v a b
+  .  Monoid v
+  => MonadEffect m
+  => (a -> Widget v a)
+  -> a
+  -> m (Widget v b)
+internalise axn val = do
+  ref <- liftEffect $ Ref.new val
+  pure (render ref)
+  where
+  render ref = liftEffect (Ref.read ref) >>= go
+    where
+    go v = do
+      v' <- axn v
+      liftEffect do Ref.write v' ref
+      go v'
 
