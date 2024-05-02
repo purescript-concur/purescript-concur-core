0a95fc2c9b18ca7a2715c30b8bb8a2dc9bf8c78d
Anupam Jain <ajnsit@gmail.com>
2024-05-03 00:37:22 +0530

    Add Wire machinery

--- src/Concur/Core/Patterns.purs
+++ src/Concur/Core/Patterns.purs
@@ -1,16 +1,23 @@
 module Concur.Core.Patterns where
 
 import Concur.Core (Result(..), Widget, mkWidget)
+import Control.Alt (class Alt, (<|>))
 import Control.Applicative (pure)
 import Control.Bind (bind, discard, (>>=))
 import Control.Monad (class Monad)
+import Control.Plus (class Plus)
 import Data.Array as Array
-import Data.Either (Either(..))
-import Data.Function (flip, ($), (>>>))
+import Data.Either (Either(..), either)
+import Data.Function (flip, ($), (<<<), (>>>))
+import Data.Functor ((<$>))
+import Data.Lens (Lens')
+import Data.Lens as L
 import Data.Monoid (class Monoid)
+import Data.Newtype (class Newtype, un)
 import Data.Semigroup ((<>))
 import Data.Traversable (traverse_)
-import Data.Unit (Unit)
+import Data.Unit (Unit, unit)
+import Effect (Effect)
 import Effect.Class (class MonadEffect, liftEffect)
 import Effect.Ref as Ref
 import Unsafe.Reference (unsafeRefEq)
@@ -84,4 +91,57 @@ internalise axn val = do
       v' <- axn v
       liftEffect do Ref.write v' ref
       go v'
+
+-- WORKING WITH LOCAL ENVIRONMENTS
+
+-- | A wire can send values up into a local environment
+newtype Wire m a = Wire
+  { value :: Effect a
+  , send :: a -> Effect Unit
+  , receive :: m a
+  }
+
+derive instance newtypeWire :: Newtype (Wire m a) _
+
+-- | Map a Lens over a Wire
+mapWire :: forall m s a. Alt m => MonadEffect m => Plus m => Lens' s a -> Wire m s -> Wire m a
+mapWire lens wire =
+  let wirerec = un Wire wire
+  in Wire { value: L.view lens <$> wirerec.value
+          , send: \a -> wirerec.value >>= (wirerec.send <<< L.set lens a)
+          , receive: L.view lens <$> wirerec.receive
+          }
+
+-- | Setup a local environment with a wire
+local :: forall v r a. Monoid v => a -> (Wire (Widget v) a -> Widget v r) -> Widget v r
+local ainit f = do
+  currentVal <- liftEffect $ Ref.new ainit
+  remoteCb <- liftEffect $ Ref.new $ pure $ case _ of
+     Completed a -> Ref.write a currentVal
+     _ -> pure unit
+  let wire = Wire
+       { value: Ref.read currentVal
+       , send: \a -> do
+           Ref.read remoteCb >>= traverse_ \cb -> cb (Completed a)
+       , receive: mkWidget \cb -> do
+           Ref.modify_ (_ <> [cb]) remoteCb
+           pure do 
+              Ref.modify_ (Array.deleteBy unsafeRefEq cb) remoteCb
+       }
+  f wire
+
+send :: forall m a. MonadEffect m => Plus m => Wire m a -> a -> m Unit
+send w = liftEffect <<< (un Wire w).send
+
+receive :: forall v a. Wire (Widget v) a -> Widget v a
+receive w = (un Wire w).receive
+
+with :: forall v r a. Monoid v => Wire (Widget v) a -> (a -> Widget v r) -> Widget v r
+with wire f = do
+  val <- liftEffect $ (un Wire wire).value
+  go val
+  where
+  go a = do
+    res <- (Left <$> f a) <|> (Right <$> receive wire)
+    either pure go res
 
